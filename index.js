b4ad1a2a1c3e35db5f001edc0db41c9de5ac483c
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-08-25 18:07:31 -0700

    feat: navbar on tablet & mobile now has toggling functionality

--- index.js
+++ index.js
@@ -3,12 +3,28 @@ let hamburger = document.querySelector('.hamburger');
 
 hamburger.addEventListener('click', () => {
     let closeIcon = document.querySelector('.close');
-  
-    hamburger.style.display = 'none';
-    closeIcon.style.display = 'block';
-   
+    let modalBackdrop = document.querySelector('.modalBackDrop');
+    let mobileAndTabletNav = document.querySelector('nav');
+    
+    let elements = [hamburger, closeIcon, modalBackdrop, mobileAndTabletNav];
+    
+    elements.forEach((el, i) => {
+        if (el === hamburger) {
+            el.style.display = 'none';
+        } else {
+            el.style.display = 'block';
+        }
+    });
 
-
+    closeIcon.addEventListener('click', () => {
+        elements.forEach((el, i) => {
+            if (el === hamburger) {
+                el.style.display = 'block';
+            } else {
+                el.style.display = 'none';
+            }
+        })
+    });
 })
 
 window.addEventListener('resize', () => {
